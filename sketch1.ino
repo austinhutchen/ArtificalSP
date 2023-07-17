2a0f63aec5444391fb94d45b8949b4909ceacf51
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-17 09:19:35 -0700

    gesture detection demo

--- sketch1.ino
+++ sketch1.ino
@@ -1,73 +1,65 @@
+/*
+  APDS-9960 - Gesture Sensor
+
+  This example reads gesture data from the on-board APDS-9960 sensor of the
+  Nano 33 BLE Sense and prints any detected gestures to the Serial Monitor.
+
+  Gesture directions are as follows:
+  - UP:    from USB connector towards antenna
+  - DOWN:  from antenna towards USB connector
+  - LEFT:  from analog pins side towards digital pins side
+  - RIGHT: from digital pins side towards analog pins side
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense
+
+  This example code is in the public domain.
+*/
 
 #include <Arduino_APDS9960.h>
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial)
-    ; // Wait for Serial Monitor to open
+  while (!Serial);
 
   if (!APDS.begin()) {
-    Serial.println("Error initializing APDS-9960 sensor.");
-    while (true)
-      ; // Stop forever
+    Serial.println("Error initializing APDS-9960 sensor!");
   }
+
+  // for setGestureSensitivity(..) a value between 1 and 100 is required.
+  // Higher values make the gesture recognition more sensitive but less accurate
+  // (a wrong gesture may be detected). Lower values makes the gesture recognition
+  // more accurate but less sensitive (some gestures may be missed).
+  // Default is 80
+  //APDS.setGestureSensitivity(80);
+
+  Serial.println("Detecting gestures ...");
 }
-
-int proximity = 0;
-int r = 0, g = 0, b = 0;
-unsigned long lastUpdate = 0;
-
 void loop() {
-
-  // Check if a proximity reading is available.
-  if (APDS.proximityAvailable()) {
-    proximity = APDS.readProximity();
-  }
-
-  // Check if a gesture reading is available
   if (APDS.gestureAvailable()) {
+    // a gesture was detected, read and print to Serial Monitor
     int gesture = APDS.readGesture();
+
     switch (gesture) {
-    case GESTURE_UP:
-      Serial.println("Detected UP gesture");
-      break;
+      case GESTURE_UP:
+        Serial.println("Detected UP gesture");
+        break;
 
-    case GESTURE_DOWN:
-      Serial.println("Detected DOWN gesture");
-      break;
+      case GESTURE_DOWN:
+        Serial.println("Detected DOWN gesture");
+        break;
 
-    case GESTURE_LEFT:
-      Serial.println("Detected LEFT gesture");
-      break;
+      case GESTURE_LEFT:
+        Serial.println("Detected LEFT gesture");
+        break;
 
-    case GESTURE_RIGHT:
-      Serial.println("Detected RIGHT gesture");
-      break;
+      case GESTURE_RIGHT:
+        Serial.println("Detected RIGHT gesture");
+        break;
 
-    default:
-      // Ignore
-      break;
+      default:
+        // ignore
+        break;
     }
-  }
-  
-
-  
-
- // Check if a color reading is available
-  if (APDS.colorAvailable()) {
-    APDS.readColor(r, g, b);
-  }
-
-  // Print updates every 100 ms
-  if (millis() - lastUpdate > 100) {
-    lastUpdate = millis();
-    Serial.print("PR=");
-    Serial.print(proximity);
-    Serial.print(" RGB=");
-    Serial.print(r);
-    Serial.print(",");
-    Serial.print(g);
-    Serial.print(",");
-    Serial.println(b);
   }
 }
