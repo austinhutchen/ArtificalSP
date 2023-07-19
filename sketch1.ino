8d24be647adee928706f853eed0ba630b5667797
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-19 07:30:48 -0700

    better sensor accuracy

--- sketch1.ino
+++ sketch1.ino
@@ -1,67 +1,150 @@
 /*
-  APDS-9960 - Gesture Sensor
+  BLE_Central_Device.ino
 
-  This example reads gesture data from the on-board APDS-9960 sensor of the
-  Nano 33 BLE Sense and prints any detected gestures to the Serial Monitor.
-
-  Gesture directions are as follows:
-  - UP:    from USB connector towards antenna
-  - DOWN:  from antenna towards USB connector
-  - LEFT:  from analog pins side towards digital pins side
-  - RIGHT: from digital pins side towards analog pins side
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE Sense 
+  as a central device and looks for a specified service and characteristic in a 
+  peripheral device. If the specified service and characteristic is found in a 
+  peripheral device, the last detected value of the on-board gesture sensor of 
+  the Nano 33 BLE Sense, the APDS9960, is written in the specified characteristic. 
 
   The circuit:
-  - Arduino Nano 33 BLE Sense
+  - Arduino Nano 33 BLE Sense. 
 
   This example code is in the public domain.
 */
 
+#include <ArduinoBLE.h>
 #include <Arduino_APDS9960.h>
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+int gesture = -1;
+int oldGestureValue = -1;   
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-
+  
   if (!APDS.begin()) {
-    Serial.println("Error initializing APDS-9960 sensor!");
+    Serial.println("* Error initializing APDS9960 sensor!");
+  } 
+
+  APDS.setGestureSensitivity(87); 
+  
+  if (!BLE.begin()) {
+    Serial.println("* Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("Nano 33 BLE (Central)"); 
+  BLE.advertise();
+
+  Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  connectToPeripheral();
+}
+
+void connectToPeripheral(){
+  BLEDevice peripheral;
+  
+  Serial.println("- Discovering peripheral device...");
+
+  do
+  {
+    BLE.scanForUuid(deviceServiceUuid);
+    peripheral = BLE.available();
+  } while (!peripheral);
+  
+  if (peripheral) {
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+    controlPeripheral(peripheral);
+  }
+}
+
+void controlPeripheral(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
+
+  if (peripheral.connect()) {
+    Serial.println("* Connected to peripheral device!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Connection to peripheral device failed!");
+    Serial.println(" ");
+    return;
   }
 
-  // for setGestureSensitivity(..) a value between 1 and 100 is required.
-  // Higher values make the gesture recognition more sensitive but less accurate
-  // (a wrong gesture may be detected). Lower values makes the gesture recognition
-  // more accurate but less sensitive (some gestures may be missed).
-  // Default is 80
-  //APDS.setGestureSensitivity(80);
+  Serial.println("- Discovering peripheral device attributes...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("* Peripheral device attributes discovered!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Peripheral device attributes discovery failed!");
+    Serial.println(" ");
+    peripheral.disconnect();
+    return;
+  }
 
-  Serial.println("Detecting gestures ...");
-  APDS.setGestureSensitivity(90);
+  BLECharacteristic gestureCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
+    
+  if (!gestureCharacteristic) {
+    Serial.println("* Peripheral device does not have gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!gestureCharacteristic.canWrite()) {
+    Serial.println("* Peripheral does not have a writable gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+  
+  while (peripheral.connected()) {
+    gesture = gestureDetectection();
+
+    if (oldGestureValue != gesture) {  
+      oldGestureValue = gesture;
+      Serial.print("* Writing value to gesture_type characteristic: ");
+      Serial.println(gesture);
+      gestureCharacteristic.writeValue((byte)gesture);
+      Serial.println("* Writing value to gesture_type characteristic done!");
+      Serial.println(" ");
+    }
+  
+  }
+  Serial.println("- Peripheral device disconnected!");
 }
-void loop() {
+  
+int gestureDetectection() {
   if (APDS.gestureAvailable()) {
-      // a gesture was detected, read and print to Serial Monitor
-    // in the future, this will listen for audio after a GESTURE_UP, GESTURE_RIGHT combination. Other gesture combinations will do ther things. Embed switch statements accordingly.
-    int gesture = APDS.readGesture();
+    gesture = APDS.readGesture();
 
     switch (gesture) {
       case GESTURE_UP:
-        Serial.println("Detected UP gesture");
+        Serial.println("- UP gesture detected");
         break;
-
       case GESTURE_DOWN:
-        Serial.println("Detected DOWN gesture");
+        Serial.println("- DOWN gesture detected");
         break;
-
       case GESTURE_LEFT:
-        Serial.println("Detected LEFT gesture");
+        Serial.println("- LEFT gesture detected");
         break;
-
       case GESTURE_RIGHT:
-        Serial.println("Detected RIGHT gesture");
+        Serial.println("- RIGHT gesture detected");
         break;
-
       default:
-        // ignore
+        Serial.println("- No gesture detected");
         break;
+      }
     }
-  }
+    return gesture;
 }
